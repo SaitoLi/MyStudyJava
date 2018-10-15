aea772a244f06a11d2a036eac8b3ad251f7f62e8
lzz <mrli1211@163.com>
2018-10-15 20:04:12 +0800

    tranning

---
+++ Study/src/com/lzz/tranning/ColorArray.java
@@ -0,0 +1,8 @@
+package com.lzz.tranning;
+
+public class ColorArray {
+	public static void main(String[] args) {
+		int[][] arrays = {{0,1,0,0},{1,0,0,1},{1,1,0,1},{0,1,0,1}};
+		
+	}
+}
--- Study/src/com/lzz/tranning/MaxSequenceSum.java
+++ Study/src/com/lzz/tranning/MaxSequenceSum.java
@@ -1,5 +1,7 @@
 package com.lzz.tranning;
 
+import java.util.Scanner;
+
 public class MaxSequenceSum {
 	/*
 	 *  动态规划经典例题：求数组的最大的连续子序列和
@@ -36,5 +38,27 @@ public class MaxSequenceSum {
 		int maxsum2 = MaxSequenceSum.maxSequenceSum(a2, a2.length);
 		System.out.println("MaxSum of a1 is " + maxsum1);
 		System.out.println("MaxSum of a2 is " + maxsum2);
+		
+//		Scanner sc = new Scanner(System.in);
+//		int len = 10;
+//		int[] nums = new int[len];
+//		for(int i = 0 ; i < len ; i++) {
+//			nums[i] = sc.nextInt();
+//		}
+//		int maxsum = maxSequenceSum(nums, nums.length);
+//		System.out.println(maxsum);
+//		sc.close();
+		
+//		Scanner sc = new Scanner(System.in);
+//		String str = sc.nextLine();
+//		String[] nums = str.split(",");
+//		nums[0] = nums[0].substring(1);
+//		nums[nums.length - 1] = nums[nums.length - 1].substring(0,nums[nums.length - 1].length() - 2);
+//		int[] array = new int[nums.length];
+//		for(int i =0  ; i < nums.length ; i++) {
+//			array[i] = Integer.parseInt(nums[i]);
+//		}
+//		System.out.println(maxSequenceSum(array, array.length));
+//		sc.close();
 	}
 }
